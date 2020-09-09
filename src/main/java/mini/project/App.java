504193bcde4f661f15a8e5b65f223707c8289bde
parkminseob <minsub.prk@gmail.com>
2020-09-09 13:13:44 +0900

    add

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -5,7 +5,7 @@ package mini.project;
 
 public class App {
   public String getGreeting() {
-    return "Hello world12345";
+    return "Hello world123456";
   }
 
   public static void main(String[] args) {
