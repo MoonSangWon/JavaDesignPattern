4b2af0d360bfb3a05808d53f23fddbc2c516e3a6
kimst0020 <kimst0020@naver.com>
2020-09-09 18:02:29 +0900

    Login -> MemberLogin Member 추가

---
+++ src/main/java/mini/project/Member.java
@@ -0,0 +1,99 @@
+package mini.project;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Member {
+
+	List<MemberLogin> members = new ArrayList<>();
+	Scanner scanner = new Scanner(System.in);
+
+	public Member() {
+		members.add(new MemberLogin("admin", "1234", "Kim"));
+	}
+
+	public void System() {
+		int num = 0;
+		while ((num = menu()) != 0) {
+			switch (num) {
+			case 1:
+				TeacherLogin();
+				break;
+			case 2:
+				TeacherJoin();
+				break;
+			case 3:
+				TeacherList();
+			}
+		}
+	}
+
+	private void TeacherJoin() {
+		String Id = Prompt.inputString("ID : ");
+		String Password = Prompt.inputString("Password : ");
+		String Name = Prompt.inputString("Name : ");
+		members.add(new MemberLogin(Id, Password, Name));
+	}
+
+	private void TeacherLogin() {
+		while (true) {
+			String Id = Prompt.inputString("ID : ");
+
+			String Password = Prompt.inputString("Password : ");
+
+			MemberLogin member = FindId(Id);
+
+			if (member == null) {
+				System.out.println("입력하신 아이디가 일치하지 않습니다.");
+				System.out.println("다시 입력하시오.");
+			} else if(member.getPassword().equals(Password)) {
+				System.out.println("학생정보관리시스템에 접속하였습니다.");
+				break;
+			} else {
+				System.out.println("입력하신 패스워드가 일치하지 않습니다.");
+				System.out.println("다시 입력하시오.");
+			}
+		}
+	}
+
+	public boolean TestId(String Id) {
+		boolean check = true;
+		MemberLogin member = FindId(Id);
+		if (member == null)
+			check = false;
+		return check;
+
+	}
+
+	private MemberLogin FindId(String Id) {
+		for(MemberLogin memberLogin : members) {
+			if(memberLogin.getId().equals(Id)) {
+				return memberLogin;
+			}
+		}
+		return null;
+	}
+
+	public int menu() {
+		return Prompt.inputInt("[1]로그인 [2]회원가입 [3]전체회원");
+	}
+
+
+
+	public void TeacherList() {
+		System.out.println("[관리자 목록]");
+
+		Iterator<MemberLogin> ite = members.iterator();
+
+		while(ite.hasNext()) {
+			System.out.println(ite.next() + " ");
+		}
+	}
+
+
+
+
+}
--- src/main/java/mini/project/Teacher.java
+++ src/main/java/mini/project/Teacher.java
@@ -1,30 +1,12 @@
 package mini.project;
 
-import java.util.Scanner;
 
 public class Teacher {
 	public static void main(String[] args) {
 		System.out.println("[학생정보관리]");
 
-		Scanner scanner = new Scanner(System.in);
-
-		while (true) {
-			String Id = Prompt.inputString("ID : ");
-
-			String Password = Prompt.inputString("Password : ");
-
-			Login l = new Login(Id, Password);
-			boolean result = l.Test();
-
-			if (result == false) {
-				System.out.println("입력하신 아이디와 패스워드가 일치하지 않습니다.");
-				System.out.println("다시 입력하시오.");
-			} else {
-				System.out.println("학생정보관리시스템에 접속하였습니다.");
-				break;
-			}
-
-		}
+		Member member = new Member();
+		member.System();	
 	}
 
 }
